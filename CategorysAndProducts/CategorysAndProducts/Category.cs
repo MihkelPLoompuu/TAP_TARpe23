2ec99cc0e615ff5ddce36215b5ee96f4c5f4f1d8
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-03-27 17:32:03 +0200

    e
    e

---
+++ CategorysAndProducts/CategorysAndProducts/Category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategorysAndProducts
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public virtual ObservableCollectionListSource<Product> Products { get; } = new();
+    }
+}
